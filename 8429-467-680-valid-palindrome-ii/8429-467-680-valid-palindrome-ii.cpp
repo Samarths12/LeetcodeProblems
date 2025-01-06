6ca118321b6da1c79ecdf3d560e214542febe0d5
Samarths12 <0312sammyy@gmail.com>
2025-01-06 19:33:30 +0530

    Time: 4 ms, Memory: 26.7 MB - LeetHub

---
+++ 8429-467-680-valid-palindrome-ii/8429-467-680-valid-palindrome-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+bool checkPalindrome(string s , int i , int j){
+    while(i<=j){
+        if(s[i]!=s[j]){
+            return false;
+        }
+            i++;
+            j--;
+        
+        
+    }
+    return true;
+}
+    bool validPalindrome(string s) {
+
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i<=j){
+            if(s[i] != s[j]){
+                //1 bar i hatao aur 1 bae left hatao
+                return checkPalindrome(s , i+1 , j) || checkPalindrome(s , i , j-1);
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+        
+    }
+};
